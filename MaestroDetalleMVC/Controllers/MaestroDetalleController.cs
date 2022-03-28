e630d1963a7192f9fa783525d8bcca297b9a5349
Miltioxs <miltioxs98@hotmail.com>
2022-03-27 19:17:09 -0600

    Creando un proyecto de ejemplo para hacer un Maestro Detalle

---
+++ MaestroDetalleMVC/Controllers/MaestroDetalleController.cs
@@ -0,0 +1,68 @@
+﻿using MaestroDetalleMVC.Models;
+using MaestroDetalleMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaestroDetalleMVC.Controllers
+{
+    public class MaestroDetalleController : Controller
+    {
+        // GET: MaestroDetalle
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(VentaViewModel model) 
+        {
+            try
+            {
+                //El using que esta aquí sirve para crear una conexion y al terminar luego de las llave la elimina.
+                using (MaestroDetalleMVCEntities db = new MaestroDetalleMVCEntities()) 
+                {
+                    //Creo mi objeto oVenta de mi modelo que esta en ViewMoldes=>Venta
+                    venta oVenta = new venta();
+                    //Calculo mi campo fecha con la función DateTime.Now, para tomar la fecha del momento(ahora).
+                    oVenta.fecha = DateTime.Now;
+                    //Recibo desde la vista el cliente y se lo asigno a mi objeto oVenta.
+                    oVenta.cliente = model.cliente;
+                    //Lo agregamos a la base de datos con db.venta.Add y le paso mi objeto de venta.
+                    db.venta.Add(oVenta);
+                    //Este sirve para guardar mi usuario.
+                    db.SaveChanges();//Si no requiero tomar el id de la venta, no es necesario que haga esto SaveChanges()
+
+                    //Recorrer los conceptos que estan llegando desde mi vista utilizando input hidden, osea Cantidad, PrecioUnitario, Nombre 
+                    //Tambien calculo el total y guardo el id de la venta en idVenta.
+                    foreach (var oC in model.conceptos) 
+                    {
+                        //Creo mi objeto oConcepto, oC tienen que ir los nombre igual a los del modelo ejemplo: oC.cantidad.
+                        concepto oConcepto = new concepto();
+                        oConcepto.cantidad = oC.cantidad;
+                        oConcepto.nombre = oC.nombre;
+                        oConcepto.precioUnitario = oC.precioUnitario;
+                        oConcepto.total = oC.cantidad * oC.precioUnitario;
+                        oConcepto.idVenta = oVenta.id;
+                        //Lo agregamos a la base de datos con db.concepto.Add y le paso mi objeto de oConcepto.
+                        db.concepto.Add(oConcepto);
+
+                    }
+                    //Guardo los cambios a mi base de datos.
+                    db.SaveChanges();
+                }
+                ViewBag.Message = "Registro Insertado";
+                //Redirecciono a mi vista index. si lo dejo como return view busca una vista como el nombre de mi accion y si no existe da error.
+                //Quite el redirectToAction y cambie el nombre mi método Index a Add.
+                    return View();
+            }
+            catch (Exception e)
+            {
+
+                return View(model);
+            }
+        }
+    }
+}
---
+++ MaestroDetalleMVC/Models/ViewModels/VentaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaestroDetalleMVC.Models.ViewModels
+{
+    public class VentaViewModel
+    {
+        public string cliente { get; set; }
+
+        //Lista de conceptos
+        public List<Concepto> conceptos { get; set; }
+    }
+
+    public class Concepto { 
+        public int cantidad { get; set; }
+        public int idVenta { get; set; }
+        public string nombre { get; set; }
+        public decimal precioUnitario { get; set; }
+        public decimal total { get; set; }
+    }
+}
